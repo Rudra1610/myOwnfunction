acf77bec02a4b0766a67763a1c25aa0291acf361
Rudra1610 <68952069+Rudra1610@users.noreply.github.com>
2020-08-19 14:49:14 +0530

    Add files via upload

---
+++ myOwnfunctions.js
@@ -0,0 +1,13 @@
+function bounceOff(rectA,rectB) {
+
+    if (rectA.x - rectB.x < rectB.width/2 + rectA.width/2
+      && rectB.x - rectA.x < rectB.width/2 + rectA.width/2) {
+    rectA.velocityX = rectA.velocityX * (-1);
+    rectB.velocityX = rectB.velocityX * (-1);
+  }
+  if (rectA.y - rectB.y < rectB.height/2 + rectA.height/2
+    && rectB.y - rectA.y < rectB.height/2 + rectA.height/2){
+    rectA.velocityY = rectA.velocityY * (-1);
+    rectB.velocityY = rectB.velocityY * (-1);
+  }
+  }
